291d6094383256c80847dc06d37fad1dfa529a05
Kewan Seymour <kewan3272001@gmail.com>
2022-08-06 11:31:17 +0200

    Fixed backend, showing pokemon images

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,18 +2,17 @@ import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
 
-export const appRouter = trpc
-  .router()
-  .query('hello', {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-      return {
-        greeting: `hello ${input?.text ?? 'world'}`,
-      };
+import { PokemonClient } from 'pokenode-ts';
+
+export const appRouter = trpc.router().query('get-pokemon', {
+    input: z.object({id: z.number()}),
+    async resolve({ input }) {
+
+      const api = new PokemonClient()
+
+      const pokemon = await api.getPokemonById(input.id)
+
+      return pokemon;
     },
   });
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,41 @@
 import { getOptionsForVote } from '@/utils/getRandomPokemon'
 import { useState, useEffect } from 'react';
 
+import { trpc } from '@/utils/trpc';
+
 export default function Home() {
-
-
-    const [options, setOpts] = useState([1, 2]);
-
-    useEffect(() => {
-            setOpts(getOptionsForVote())
-        },[])
-
-    const [first, second] = options;
-
     
-
-
-
+    const [options, setOpts] = useState([1, 2]);
+    
+    useEffect(() => {
+        setOpts(getOptionsForVote())
+        }, []);
+    
+    const [first, second] = options;
+    
+    const first_pokemon = trpc.useQuery(['get-pokemon', { id: first }]);
+    const second_pokemon = trpc.useQuery(['get-pokemon', { id: second }]);
+    
+    if (first_pokemon.isLoading || second_pokemon.isLoading) return "Loading"
+        
+    console.log(first, second)
+    console.log(first_pokemon.data)
+        
+        
+        
     return (
         <div className="h-screen w-screen flex flex-col justify-center align items-center">
-            <div className="text-2xl text-center">Which pokemon is rounder?</div>
-            <div className="p-2"/>
-            <div className="border rounder p-8 flex justify-between max-w-2xl items-center">
-                <div className="w-16 h-16 bg-red-800">{first}</div>
-                <div className="p-8">VS</div>
-                <div className="w-16 h-16 bg-red-800">{second}</div>
-            </div>
+        <div className="text-2xl text-center">Which pokemon is rounder?</div>
+        <div className="p-2"/>
+        <div className="border rounder p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-64 h-64 flex flex-col"><img className='w-full' src={first_pokemon.data?.sprites?.front_default!} />
+        <div className='text-center capitalize' >{first_pokemon.data?.name}</div></div>
+        <div className="p-8">VS</div>
+        <div className="w-64 h-64 flex flex-col"><img className='w-full' src={second_pokemon.data?.sprites?.front_default!} />
+        <div className='text-center capitalize' >{second_pokemon.data?.name}</div></div>
+        </div>
         </div>
     );
 };
-
+        
+        
