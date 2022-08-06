fa932b447f2cfbeea8512e9168a61575560d56ca
Kewan Seymour <kewan3272001@gmail.com>
2022-08-06 15:16:58 +0200

    mede text green

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
                     <img className='w-full' src={second_pokemon.data?.sprites?.front_default!} />
                     <div className='text-center capitalize mt-[-2rem]' >{second_pokemon.data?.name}</div>
                     <button className={BTN} onClick={() => vote_for_roundest(second)} >Rounder</button>
-                    <>Hi im new</>
+                    <p className='text-green-700' >Something else</p>
                 </div>
             </div>
         </div>
