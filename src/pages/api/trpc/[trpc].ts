2e4e590c8c4741c112ea5e0a2e4e809eb4635a68
Kewan Seymour <kewan3272001@gmail.com>
2022-08-06 22:50:24 +0200

    Add prisma and voting endpoint

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,20 +1,35 @@
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
+import { prisma } from '../../../utils/db';
 
 import { PokemonClient } from 'pokenode-ts';
 
 export const appRouter = trpc.router().query('get-pokemon', {
-    input: z.object({id: z.number()}),
-    async resolve({ input }) {
+  input: z.object({ id: z.number() }),
+  async resolve({ input }) {
 
-      const api = new PokemonClient()
+    const api = new PokemonClient()
 
-      const pokemon = await api.getPokemonById(input.id)
+    const pokemon = await api.getPokemonById(input.id)
 
-      return {name: pokemon.name, sprites: pokemon.sprites};
-    },
-  });
+    return { name: pokemon.name, sprites: pokemon.sprites };
+  },
+}).mutation("cast_vote", {
+  input: z.object({
+    votedFor: z.number(),
+    votedAgainst: z.number(),
+  }),
+  async resolve({ input }) {
+
+    const write_vote_db = await prisma.vote.create({
+      data:
+        input
+    });
+
+    return { success: true, vote: write_vote_db };
+  },
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
